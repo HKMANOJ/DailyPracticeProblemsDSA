a1d81276a985bf23c7eee540d888a340ddf4d4e7
HKMANOJ <97114741+HKMANOJ@users.noreply.github.com>
2023-09-10 10:26:02 +0530

    Create insert.java

---
+++ insert.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    // Function to insert a node in a BST.
+    Node insert(Node root, int Key) {
+        // your code here
+        if(root==null)return new Node(Key);
+        if(root.data>Key) root.left = insert(root.left,Key);
+        if(root.data<Key) root.right = insert(root.right,Key);
+        
+        return root;
+    }
+}
