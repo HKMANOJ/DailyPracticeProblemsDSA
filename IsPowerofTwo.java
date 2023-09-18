ce4816c4d335d9d1da6f7a839ecc76dc25ea6938
HKMANOJ <97114741+HKMANOJ@users.noreply.github.com>
2023-09-18 19:06:11 +0530

    Create IsPowerofTwo.java

---
+++ IsPowerofTwo.java
@@ -0,0 +1,11 @@
+class Solution{
+    
+    // Function to check if given number n is a power of two.
+    public static boolean isPowerofTwo(long n){
+        
+        // Your code here
+        if(n==0) return false;
+        return ((n&(n-1))==0);
+    }
+    
+}
