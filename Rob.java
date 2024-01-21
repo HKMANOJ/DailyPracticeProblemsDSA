be65fe488eb21fc86bc64d6be8b9717d635fd143
HKMANOJ <97114741+HKMANOJ@users.noreply.github.com>
2024-01-21 11:56:39 +0530

    Rob.java

---
+++ Rob.java
@@ -0,0 +1,18 @@
+// Input: nums = [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+class Solution {
+    public int rob(int[] nums) {
+        int rob = 0;
+        int norob = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            int newRob = norob + nums[i];
+            int maxrob = Math.max(norob, rob);
+            rob = newRob;
+            norob = maxrob;
+        }
+        return Math.max(rob, norob);
+    }
+  
+}
