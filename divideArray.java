003d3864bd1a8549720d8edf63abd28976083508
HKMANOJ <97114741+HKMANOJ@users.noreply.github.com>
2024-02-01 20:27:52 +0530

    Create divideArray.java

---
+++ divideArray.java
@@ -0,0 +1,19 @@
+// Input: nums = [1,3,4,8,7,9,3,5,1], k = 2
+// Output: [[1,1,3],[3,4,5],[7,8,9]]
+// Explanation: We can divide the array into the following arrays: [1,1,3], [3,4,5] and [7,8,9].
+// The difference between any two elements in each array is less than or equal to 2.
+// Note that the order of elements is not important.
+
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+                Arrays.sort(nums);
+        int[][] ans = new int[nums.length / 3][3];
+        for (int i = 0; i < nums.length; i += 3) {
+            if (nums[i + 2] - nums[i] > k) {
+                return new int[0][0];
+            }
+            ans[i / 3] = new int[]{nums[i], nums[i + 1], nums[i + 2]};
+        }
+        return ans;
+    }
+}
