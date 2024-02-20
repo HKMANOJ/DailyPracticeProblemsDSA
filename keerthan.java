582824c33544fb3dd5de95047858d62507fc3c19
HKMANOJ <97114741+HKMANOJ@users.noreply.github.com>
2024-02-20 19:31:08 +0530

    Update keerthan.java

---
+++ keerthan.java
@@ -0,0 +1,17 @@
+class keerthan{
+
+void keerthan(){
+
+System.out.print("i am keertan");
+}
+
+void keerthan(String father, String mother) 
+ {
+System.out.print (" my father name" + father)
+}
+
+public static void main(){
+
+keerthan ob = new Keertan()
+
+}
