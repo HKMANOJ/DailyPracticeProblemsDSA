a7cf5057b3d984f0918a83a234bc59c4c4d3cef9
HKMANOJ <97114741+HKMANOJ@users.noreply.github.com>
2024-03-08 14:56:25 +0530

    Create maxFrequencyElements.java

---
+++ maxFrequencyElements.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+              Map<Integer, Integer> frequencies = new HashMap<>();
+        for (int num : nums) {
+            frequencies.put(num, frequencies.getOrDefault(num, 0) + 1);
+        }
+
+    
+        int maxFrequency = 0;
+        for (int frequency : frequencies.values()) {
+            maxFrequency = Math.max(maxFrequency, frequency);
+        }
+
+        // Calculate the total frequencies of elements with the maximum frequency
+        int frequencyOfMaxFrequency = 0;
+        for (int frequency : frequencies.values()) {
+            if (frequency == maxFrequency) {
+                frequencyOfMaxFrequency++;
+            }
+        }
+        return frequencyOfMaxFrequency * maxFrequency;
+    
+    }
+}
