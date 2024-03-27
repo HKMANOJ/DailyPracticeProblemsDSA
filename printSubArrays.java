7fb9c703cb1c2b68fdd75b7d7ccd9e5dcfb6adc9
HKMANOJ <97114741+HKMANOJ@users.noreply.github.com>
2024-03-27 20:17:30 +0530

    Create printSubArrays.java

---
+++ printSubArrays.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class Subarry {
+    static void printSubArrays(int[] arr, int start, int end)
+    {
+        // Stop if we have reached the end of the array
+        if (end == arr.length)
+            return;
+            // Increment the end point and start from 0
+        else if (start > end)
+            printSubArrays(arr, 0, end + 1);
+
+        else {
+            System.out.print("[");
+            for (int i = start; i < end; i++)
+                System.out.print(arr[i] + ", ");
+            System.out.println(arr[end] + "]");
+            printSubArrays(arr, start + 1, end);
+        }
+        return;
+    }
+
+    public static void main(String args[])
+    {
+        int[] arr = { 1, 2, 3 };
+        printSubArrays(arr, 0, 0);
+    }
+}
